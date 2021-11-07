c1cabc1c7f1084c7c169ba2e90f57a5670087728
mirayoo <66087956+mirayoo@users.noreply.github.com>
2021-11-07 22:37:12 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+//Tabs
+$(".tab").click(function() {
+	$("#catalog .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(2).addClass("active");
+
+//Size selection
+
 $( '.sizeselect' ).click (function() {
     $( this ).not('.inactive').addClass( 'active' ).siblings().removeClass( 'active' )
 });
+
+//Image Slider
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    speed: 600,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      }
+  });
