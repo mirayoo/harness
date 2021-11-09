f0b6f971019cdefd3d83af0bbc22f496b36287ec
mirayoo <66087956+mirayoo@users.noreply.github.com>
2021-11-09 17:38:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//MOBILE MENU
+
+$(".navToggle").on("click", function(){
+    $(this).toggleClass("open");
+  $("#mobile-menu").toggleClass("active");
+  })
+
 //Tabs
 $(".tab").click(function() {
 	$("#catalog .tab").removeClass("active").eq($(this).index()).addClass("active");
@@ -21,4 +28,20 @@ var swiper = new Swiper(".swiper", {
         delay: 4000,
         disableOnInteraction: false,
       }
+  });
+
+  //Popup
+
+  $(document).ready(function(){
+    $('.card-btn').click(function(){ 
+      var popupBlock = $('#'+$(this).data('popup'));
+      popupBlock.addClass('active')
+        $('.popup').click(function(){
+          popupBlock.css('opacity','0').find('.popup-content').css('margin-top','350px');  
+          setTimeout(function(){
+            $('.popup').removeClass('active');
+            popupBlock.css('opacity','').find('.popup-content').css('margin-top','');
+          }, 600);
+        });
+   });
   });
