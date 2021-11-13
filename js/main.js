82ca6f709882420e3b87e460684e77f2fb98a192
mirayoo <66087956+mirayoo@users.noreply.github.com>
2021-11-13 21:33:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(".tab").click(function() {
 //Size selection
 
 $( '.sizeselect' ).click (function() {
-    $( this ).not('.inactive').addClass( 'active' ).siblings().removeClass( 'active' )
+    $( this ).not('.inactive').toggleClass( 'active' ).siblings().removeClass( 'active' )
 });
 
 //Image Slider
@@ -32,16 +32,32 @@ var swiper = new Swiper(".swiper", {
 
   //Popup
 
-  $(document).ready(function(){
-    $('.card-btn').click(function(){ 
-      var popupBlock = $('#'+$(this).data('popup'));
-      popupBlock.addClass('active')
-        $('.popup').click(function(){
-          popupBlock.css('opacity','0').find('.popup-content').css('margin-top','350px');  
-          setTimeout(function(){
-            $('.popup').removeClass('active');
-            popupBlock.css('opacity','').find('.popup-content').css('margin-top','');
-          }, 600);
-        });
-   });
+  $('.card a.card-btn').click(function(event) {
+    $('#product-btn').modal({
+      fadeDuration: 250,
+      fadeDelay: 1,
+      closeClass: 'icon-remove',
+      closeText: '!'
+    });
+    return false;
   });
+
+
+// Popup Content
+
+$('.card-btn').on('click', function(){
+    
+  $('#product-btn .popup-img').html('<img src="'+$(this).closest('.card').find('img').attr('src')+'" />');
+  $('#product-btn .popup-title h3').html($(this).closest('.card').find('h3').text());
+  $('#product-btn .old-price').html($(this).closest('.card').find('.old-price').text());
+  $('#product-btn .new-price').html($(this).closest('.card').find('.new-price').text());
+  $('#product-btn .popup-size span').html($(this).closest('.card').find('.sizeselect.active').text());
+});
+
+$('.sizeselect').on('click',function(){
+ if ($(this).closest('.sizeselect').hasClass('active')) {
+  $(this).closest('.card').find('.card-btn').removeClass('disabled');
+} else {
+  $(this).closest('.card').find('.card-btn').addClass('disabled');
+}
+});
